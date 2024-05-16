1862dec3055a73d9395d29ea8a196364bb667a16
serafinpl <ser.burberibar@gmail.com>
2024-05-17 00:43:03 +0200

    install ui and add siple nav bar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import PrelineScript from "@/components/PrelineScript";
+import NavBar from "@/components/nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <PrelineScript />
+      <NavBar/>
+      
+        {children}
+      </body>
     </html>
   );
 }
