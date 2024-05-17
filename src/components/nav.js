b1324937bab52efd4f3ea162a5ee62add2377039
serafinpl <ser.burberibar@gmail.com>
2024-05-17 02:58:03 +0200

    all items on nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import NavButton from "./navButton";
+import NavUSer from './navUser';
 
 import techLogo from "../../public/TestLogo.svg";
 import homeIco from "../../public/home.svg";
@@ -8,22 +9,22 @@ import creditIco from "../../public/credit.svg";
 import chatIco from "../../public/chat.svg";
 import calendarIco from "../../public/calendar.svg";
 
+import drAvatar from "../../public/senior-woman-doctor.png"
+
 
 const NavBar = () =>
-(<header className="flex flex-wrap lg:justify-start lg:flex-nowrap  bg-white text-sm py-4 mx-3 my-3 radius70">
-    <nav className="max-w-[85rem] w-full mx-auto px-4 flex flex-wrap basis-full items-center justify-between radius70" aria-label="Global">
+(<header className="flex flex-wrap xl:justify-start xl:flex-nowrap  bg-white text-sm p-4 m-3 radius70">
+    <nav className="px-4 w-full flex flex-wrap basis-full items-center justify-between radius70" aria-label="Global">
         <Image src={techLogo} />
-        <div className="lg:order-3 flex items-center gap-x-2">
-            <button type="button" className="lg:hidden hs-collapse-toggle p-2.5 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none " data-hs-collapse="#navbar-alignment" aria-controls="navbar-alignment" aria-label="Toggle navigation">
+        <div className="xl:order-3 flex items-center gap-x-2">
+            <button type="button" className="xl:hidden hs-collapse-toggle p-2.5 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none " data-hs-collapse="#navbar-alignment" aria-controls="navbar-alignment" aria-label="Toggle navigation">
                 <svg className="hs-collapse-open:hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6" /><line x1="3" x2="21" y1="12" y2="12" /><line x1="3" x2="21" y1="18" y2="18" /></svg>
                 <svg className="hs-collapse-open:block hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
             </button>
-            <button type="button" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none ">
-                Button
-            </button>
+            <NavUSer userAvatar={drAvatar} name='Dr. Jose Simmons' role='General Practitioner'/>
         </div>
-        <div id="navbar-alignment" className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow lg:grow-0 lg:basis-auto lg:block lg:order-2">
-            <div className="flex flex-col gap-5 mt-5 lg:flex-row lg:items-center lg:mt-0 lg:ps-5">
+        <div id="navbar-alignment" className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow xl:grow-0 xl:basis-auto xl:block xl:order-2">
+            <div className="flex flex-col gap-5 mt-5 xl:flex-row xl:items-center xl:mt-0 xl:ps-5">
                 <NavButton ico={homeIco}>Overview</NavButton>
                 <NavButton ico={groupIco} active>Patients</NavButton>
                 <NavButton ico={calendarIco}>Schedule</NavButton>
