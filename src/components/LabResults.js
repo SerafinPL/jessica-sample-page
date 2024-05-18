1370162e19dc48aa241858246f60f94fa6629b02
serafinpl <ser.burberibar@gmail.com>
2024-05-18 19:37:52 +0200

    reorganize folder

---
+++ src/components/LabResults.js
@@ -0,0 +1,11 @@
+
+
+import CardTitle from "@/components/CardTitle";
+
+const LabResults = () => (
+    <div class="flex flex-col bg-white rounded-xl ">
+        <CardTitle  title='Lab Results'></CardTitle>
+    </div>
+);
+
+export default LabResults
