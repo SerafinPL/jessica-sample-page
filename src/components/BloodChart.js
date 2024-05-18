e687baab2b9705abe954c565d0831d3e91a76928
serafinpl <ser.burberibar@gmail.com>
2024-05-18 19:15:24 +0200

    add bloodchart sample and loading component add files

---
+++ src/components/BloodChart.js
@@ -0,0 +1,14 @@
+
+import Image from 'next/image';
+import Loading from './Loading';
+
+
+
+
+const BloodChart = () => (
+    <div className={`w-full flex justify-center items-center bg-secRow-fiolet rounded-xl`} >
+        <Loading/>
+    </div>
+);
+
+export default BloodChart
