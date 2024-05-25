078f3ea174c03e2db1b03f2adaea0932f894f0ae
serafinpl <ser.burberibar@gmail.com>
2024-05-26 00:22:59 +0200

    styling ico text done

--- src/components/PatientIcoText.js
+++ src/components/PatientIcoText.js
@@ -3,13 +3,12 @@ import Image from 'next/image';
 const PatientIcoText = ({ textUp, textDown, ico }) => {
 
     return (
-        <div className="flex items-center ml-5">
+        <div className="flex items-center ml-5 mt-6	">
             {ico && <Image src={ico} width={42} height={42} className={``} />}
-            <div className=" mx-4">
-                {textUp && <p className="body-title-14 text-center ">{textUp}</p>}
-                {textDown && <p className="body-title-bold-14 text-center ">{textDown}</p>}
-            </div>
-
+            <div className=" mx-4 ml-8	">
+                {textUp && <p className="body-title-14 ">{textUp}</p>}
+                {textDown && <p className="body-title-bold-14 ">{textDown}</p>}
+            </div>            
         </div>
     )
 };
--- src/components/PatientInfo.js
+++ src/components/PatientInfo.js
@@ -1,18 +1,31 @@
 import Image from 'next/image';
 import PatientIcoText from './PatientIcoText';
+
+import FemaleIcon from "@public/FemaleIcon.svg";
+import MaleIcon from "@public/MaleIcon.svg";
+import PhoneIcon from "@public/PhoneIcon.svg";
 import BirthIcon from "@public/BirthIcon.svg";
 
 
 const PatientInfo = ({ user }) => {
 
+    const gender = user.gender == 'Male' ? MaleIcon : FemaleIcon;  
+
     return (
-        <div className="flex flex-col bg-white rounded-xl  h-[740px] ">
+        <div className="flex flex-col bg-white rounded-xl mb-6 h-[740px] ">
 
             <div className="pt-8 mx-auto	">
                 {user && <Image src={user.profile_picture} width={200} height={200} className={``} />}
                 {user && <p className="card-title-24pt text-center mt-6">{user.name}</p>}
             </div>
-            <PatientIcoText ico={BirthIcon} textDown={'data'} textUp={'name'}/>
+            <PatientIcoText ico={BirthIcon} textDown={user.date_of_birth} textUp={'Date Of Birth'}/>
+            <PatientIcoText ico={user && gender} textDown={user.gender} textUp={'Gender'}/>
+            <PatientIcoText ico={PhoneIcon} textDown={user.phone_number} textUp={'Contact Info.'}/>
+            <PatientIcoText ico={PhoneIcon} textDown={user.emergency_contact} textUp={'Emergency Contacts'}/>
+            <PatientIcoText ico={BirthIcon} textDown={user.insurance_type} textUp={'Insurance Provider'}/>
+            <button type="button" class="py-3 px-4 my-8	mx74 rounded-full border border-transparent bg-thrRow-active ">
+                <p className="text-center body-emphasized-14pt">Show All Information</p>
+            </button>
         </div>
     )
 };
