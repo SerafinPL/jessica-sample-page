b75b25730f76b405771a451124099348cffbaa51
serafinpl <ser.burberibar@gmail.com>
2024-05-25 22:29:13 +0200

    add component

---
+++ src/components/PatientIcoText.js
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+
+const PatientIcoText = ({ user }) => {
+
+    return (
+        <div className="flex flex-col bg-white rounded-xl  h-[740px] ">
+
+            <div className="pt-8 mx-auto	">
+                {user && <Image src={user.profile_picture} width={200} height={200} className={``} />}
+                {user && <p className="card-title-24pt text-center mt-6">{user.name}</p>}
+            </div>
+        </div>
+    )
+};
+
+export default PatientIcoText
