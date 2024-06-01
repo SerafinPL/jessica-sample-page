f4e6fb02727fdb9528293d5f202417283d7b5fad
serafinpl <ser.burberibar@gmail.com>
2024-06-01 17:11:14 +0200

    layout fixing

--- src/components/Diagnostic/DiagnosticList.js
+++ src/components/Diagnostic/DiagnosticList.js
@@ -6,12 +6,12 @@ import CardTitle from "@/components/CardTitle";
 import ListView from "./DiagnosticView";
 
 const DiagnosticList = () => (
-        <div className="flex flex-col bg-white rounded-xl max-h-[300px] overflow-y-hidden	 ">
+        <div className="flex flex-col bg-white rounded-xl  overflow-y-hidden	 ">
             <div className="mt18">
                 <CardTitle title='Diagnostic List'></CardTitle>
             </div>
             <div className="">
-                <div className={` grid grid-cols-7 h-full mx-4 my-5 py-3.5 pl-5 bg-firstRow-A1 radius70 body-emphasized-14pt`}>
+                <div className={` grid grid-cols-7 mx-4 my-5 py-3.5 pl-5 bg-firstRow-A1 radius70 body-emphasized-14pt`}>
                     <div className={`col-span-2 `}>Problem/Diagnosis</div>
                     <div className={`col-span-3 `}>Description</div>
                     <div className={`col-span-1 `}>Status</div>
