d94694ddf1a2b869f5e615fc624d5f57a77d85d6
serafinpl <ser.burberibar@gmail.com>
2024-06-01 15:03:20 +0200

    code rewiev: change folder and make betterScrolls component

--- src/components/PatientsList/PatientsList.js
+++ src/components/PatientsList/PatientsList.js
@@ -1,36 +1,25 @@
 
-import { getPatients} from '@/data/getData';
+import { getPatients } from '@/data/getData';
 
 import CardTitle from "@/components/CardTitle";
 import PatientListView from "./PatientListView";
+import BetterScrolls from '../BetterScrolls';
 import search from "@public/search.svg";
-import { patients } from "@/data/Patients";
 
 const PatientsList = async () => {
 
-    // const viewElements = patients.map(el => (
-    //     <PatientListView avatar={el.avatar} name={el.name} gender={el.gender} age={el.age} active={el.active}/>
-    // ))
-
     const viewElements = await getPatients().then(res => {
         return res.map(el => (
-            <PatientListView avatar={el.profile_picture} name={el.name} gender={el.gender} age={el.age} active={el.name == 'Jessica Taylor'}/>
+            <PatientListView avatar={el.profile_picture} name={el.name} gender={el.gender} age={el.age} active={el.name == 'Jessica Taylor'} />
         ))
     })
 
-    const extendedElements = viewElements.concat(viewElements).concat(viewElements).concat(viewElements);
-
     return (
         <div className="flex flex-col bg-white rounded-xl  max-h-[1054px] ">
-            
             <CardTitle ico={search} title='Patients'></CardTitle>
-            <div className="overflow-y-auto [&::-webkit-scrollbar]:w-1.5
-                [&::-webkit-scrollbar-track]:bg-firstRow-scrollFront
-                [&::-webkit-scrollbar-track]:rounded-full
-                [&::-webkit-scrollbar-thumb]:bg-firstRow-A5
-                [&::-webkit-scrollbar-thumb]:rounded-full">
-                {extendedElements}
-            </div>
+            <BetterScrolls >
+                {viewElements}
+            </BetterScrolls>
         </div>
     )
 };
