234c2e62cb348ceaf9e358d61d4bf1e02c2984ab
serafinpl <ser.burberibar@gmail.com>
2024-06-01 15:16:51 +0200

    code rewiev: charts comonents

--- src/components/BetterScrolls.js
+++ src/components/BetterScrolls.js
@@ -1,17 +1,14 @@
 
 
-const BetterScrolls =  ({children, classes}) => {
-
-    return (     
-            <div className={`overflow-y-auto [&::-webkit-scrollbar]:w-1.5
+const BetterScrolls = ({ children, classes }) => (
+    <div className={`overflow-y-auto [&::-webkit-scrollbar]:w-1.5
                 [&::-webkit-scrollbar-track]:bg-firstRow-scrollFront
                 [&::-webkit-scrollbar-track]:rounded-full
                 [&::-webkit-scrollbar-thumb]:bg-firstRow-A5
                 [&::-webkit-scrollbar-thumb]:rounded-full 
                 ${classes ? classes : ''}`}>
-                {children}
-            </div>
-    )
-};
+        {children}
+    </div>
+);
 
 export default BetterScrolls
--- src/components/Chart/ChartLegend.js
+++ src/components/Chart/ChartLegend.js
@@ -3,15 +3,12 @@ import { Suspense } from "react";
 import Loading from '../Loading';
 import ChartCard from "./ChartCard";
 
-const ChartLegend = () => {
-
-    return (
+const ChartLegend = () => (
         <div className={`w-full  flex flex-col justify-start items-start max-w-[200px] mx-auto `} >
             <div>
                 <div className="inline-flex  items-center">
                     <span className="size-2 inline-block bg-thrRow-chartBlue rounded-full me-2"></span>
                     <span className="body-emphasized-14pt m-2">Systolic </span>
-
                 </div>
                 <Suspense fallback={<Loading />}>
                     <ChartCard title='Systolic' />
@@ -22,14 +19,12 @@ const ChartLegend = () => {
                 <div className="inline-flex  items-center">
                     <span className="size-2 inline-block bg-thrRow-chartPink rounded-full me-2"></span>
                     <span className="body-emphasized-14pt m-1">Diastolic </span>
-
                 </div>
                 <Suspense fallback={<Loading />}>
                     <ChartCard title='Diastolic' />
                 </Suspense>
             </div>
         </div>
-    )
-};
+    );
 
 export default ChartLegend
